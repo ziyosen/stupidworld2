379e992f297c51644e7fa3945ccb0ae0405ba20e
StupidX2 <luckyzp77@gmail.com>
2025-05-08 15:03:15 +0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -22,11 +22,11 @@ async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
         .var("UUID")
         .map(|x| Uuid::parse_str(&x.to_string()).unwrap_or_default())?;
     let host = req.url()?.host().map(|x| x.to_string()).unwrap_or_default();
-    let main_page_url = env.var("MAIN_PAGE_URL").map(|x|x.to_string()).unwrap();
-    let sub_page_url = env.var("SUB_PAGE_URL").map(|x|x.to_string()).unwrap();
-    let link_page_url = env.var("LINK_PAGE_URL").map(|x|x.to_string()).unwrap();
-    let sitemap_url = env.var("SITEMAP_URL").map(|x|x.to_string()).unwrap();
-    let convert_page_url = env.var("CONVERT_PAGE_URL").map(|x|x.to_string()).unwrap();
+    let main_page_url = env.var("MAIN_PAGE_URL").map(|x| x.to_string()).unwrap();
+    let sub_page_url = env.var("SUB_PAGE_URL").map(|x| x.to_string()).unwrap();
+    let link_page_url = env.var("LINK_PAGE_URL").map(|x| x.to_string()).unwrap();
+    let converter_page_url = env.var("CONVERTER_PAGE_URL").map(|x| x.to_string()).unwrap();
+
     let config = Config { 
         uuid, 
         host: host.clone(), 
@@ -35,18 +35,16 @@ async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
         main_page_url, 
         sub_page_url,
         link_page_url,
-        convert_page_url,
-        sitemap_url
+        converter_page_url,
     };
 
     Router::with_data(config)
         .on_async("/", fe)
         .on_async("/sub", sub)
         .on_async("/link", link)
-        .on_async("/convert", convert)
-        .on_async("/sitemap.xml", sitemap)
+        .on_async("/convert", converter)
         .on_async("/:proxyip", tunnel)
-        .on_async("/Stupid-World/:proxyip", tunnel)
+        .on_async("/Inconigto-Mode/:proxyip", tunnel)
         .run(req, env)
         .await
 }
@@ -69,48 +67,19 @@ async fn link(_: Request, cx: RouteContext<Config>) -> Result<Response> {
     get_response_from_url(cx.data.link_page_url).await
 }
 
-async fn convert(_: Request, cx: RouteContext<Config>) -> Result<Response> {
-    get_response_from_url(cx.data.convert_page_url).await
-}
-
-async fn sitemap(_: Request, cx: RouteContext<Config>) -> Result<Response> {
-    let kv = cx.kv("SITEMAP")?;
-    let github_sitemap_url = "https://raw.githubusercontent.com/xyzsds/tx/refs/heads/main/sitemap.xml";
-    if let Some(cached) = kv.get("sitemap_content").text().await? {
-        let mut headers = Headers::new();
-        headers.set("Content-Type", "application/xml")?;
-        return Ok(Response::ok(cached)?.with_headers(headers));
-    }
-    
-    let mut res = Fetch::Url(Url::parse(github_sitemap_url)?)
-        .send()
-        .await?;
-    
-    if res.status_code() == 200 {
-        let sitemap_content = res.text().await?;
-        
-        kv.put("sitemap_content", &sitemap_content)?
-            .expiration_ttl(60 * 60 * 24) // 24 jam
-            .execute()
-            .await?;
-        
-        let mut headers = Headers::new();
-        headers.set("Content-Type", "application/xml")?;
-        Ok(Response::ok(sitemap_content)?.with_headers(headers))
-    } else {
-        Response::error("Failed to fetch sitemap from GitHub", 502)
-    }
+async fn converter(_: Request, cx: RouteContext<Config>) -> Result<Response> {
+    get_response_from_url(cx.data.converter_page_url).await
 }
 
 async fn tunnel(req: Request, mut cx: RouteContext<Config>) -> Result<Response> {
     let mut proxyip = cx.param("proxyip").unwrap().to_string();
     if PROXYKV_PATTERN.is_match(&proxyip)  {
-        let kvid_list: Vec<String> = proxyip.split(",").map(|s|s.to_string()).collect();
+        let kvid_list: Vec<String> = proxyip.split(",").map(|s| s.to_string()).collect();
         let kv = cx.kv("SIREN")?;
         let mut proxy_kv_str = kv.get("proxy_kv").text().await?.unwrap_or("".to_string());
         let mut rand_buf = [0u8, 1];
         getrandom::getrandom(&mut rand_buf).expect("failed generating random number");
-        
+
         if proxy_kv_str.len() == 0 {
             console_log!("getting proxy kv from github...");
             let req = Fetch::Url(Url::parse("https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/kvProxyList.json")?);
@@ -122,14 +91,12 @@ async fn tunnel(req: Request, mut cx: RouteContext<Config>) -> Result<Response> 
                 return Err(Error::from(format!("error getting proxy kv: {}", res.status_code())));
             }
         }
-        
+
         let proxy_kv: HashMap<String, Vec<String>> = serde_json::from_str(&proxy_kv_str)?;
-        
-        // select random KV ID
+
         let kv_index = (rand_buf[0] as usize) % kvid_list.len();
         proxyip = kvid_list[kv_index].clone();
-        
-        // select random proxy ip
+
         let proxyip_index = (rand_buf[0] as usize) % proxy_kv[&proxyip].len();
         proxyip = proxy_kv[&proxyip][proxyip_index].clone().replace(":", "-");
     }
@@ -142,19 +109,19 @@ async fn tunnel(req: Request, mut cx: RouteContext<Config>) -> Result<Response> 
             }
         }
     }
-    
+
     let upgrade = req.headers().get("Upgrade")?.unwrap_or("".to_string());
     if upgrade == "websocket".to_string() {
         let WebSocketPair { server, client } = WebSocketPair::new()?;
         server.accept()?;
-    
+
         wasm_bindgen_futures::spawn_local(async move {
             let events = server.events().unwrap();
             if let Err(e) = ProxyStream::new(cx.data, &server, events).process().await {
                 console_log!("[tunnel]: {}", e);
             }
         });
-    
+
         Response::from_websocket(client)
     } else {
         Response::from_html("hi from wasm!")
