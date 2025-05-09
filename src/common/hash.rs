5f279f8c0b91447e5097f169dbb1cfe5a9404dad
StupidX2 <luckyzp77@gmail.com>
2025-05-10 00:11:39 +0700

    Update hash.rs

--- src/common/hash.rs
+++ src/common/hash.rs
@@ -1,13 +1,10 @@
 use sha2::{Digest, Sha256};
 
-/// Trait untuk hash yang menghasilkan 32-byte output
-trait Hasher32: Clone {
+trait Hasher {
     fn update(&mut self, data: &[u8]);
     fn finalize(&mut self) -> [u8; 32];
 }
 
-/// Implementasi Hasher32 untuk Sha256
-#[derive(Clone)]
 struct Sha256Hash(Sha256);
 
 impl Sha256Hash {
@@ -16,113 +13,93 @@ impl Sha256Hash {
     }
 }
 
-impl Hasher32 for Sha256Hash {
+impl Hasher for Sha256Hash {
     fn update(&mut self, data: &[u8]) {
         self.0.update(data);
     }
 
     fn finalize(&mut self) -> [u8; 32] {
-        self.0.clone().finalize().into()
+        self.0.finalize().into()
     }
 }
 
-/// Recursive HMAC-like hashing
-#[derive(Clone)]
-struct RecursiveHash<H: Hasher32 + Clone> {
-    inner: H,
-    outer: H,
+struct RecursiveHash {
+    inner: Box<dyn Hasher>,
+    outer: Box<dyn Hasher>,
+    ipad: [u8; 64],
     opad: [u8; 64],
 }
 
-impl<H: Hasher32 + Clone> RecursiveHash<H> {
-    fn new(key: &[u8], base: H) -> Self {
-        assert!(key.len() <= 64, "Key too long");
+impl RecursiveHash {
+    fn new(key: &[u8], hash: Box<dyn Hasher>) -> Self {
+        let mut ipad = [0u8; 64];
+        let mut opad = [0u8; 64];
 
-        let mut ipad = [0x36u8; 64];
-        let mut opad = [0x5cu8; 64];
+        ipad[..key.len()].copy_from_slice(&key);
+        opad[..key.len()].copy_from_slice(&key);
 
-        for i in 0..key.len() {
-            ipad[i] ^= key[i];
-            opad[i] ^= key[i];
+        for b in ipad.iter_mut() {
+            *b ^= 0x36;
         }
 
-        let mut inner = base.clone();
-        inner.update(&ipad);
+        for b in opad.iter_mut() {
+            *b ^= 0x5c;
+        }
 
-        let mut outer = base;
-        // opad disimpan, update dilakukan saat finalize
+        let mut inner = hash;
+        inner.update(&ipad);
 
         Self {
             inner,
-            outer,
+            outer: hash,
+            ipad,
             opad,
         }
     }
 }
 
-impl<H: Hasher32 + Clone> Hasher32 for RecursiveHash<H> {
+impl Hasher for RecursiveHash {
     fn update(&mut self, data: &[u8]) {
         self.inner.update(data);
     }
 
     fn finalize(&mut self) -> [u8; 32] {
-        let inner_result = self.inner.finalize();
+        let result: [u8; 32] = self.inner.finalize();
         self.outer.update(&self.opad);
-        self.outer.update(&inner_result);
+        self.outer.update(&result);
         self.outer.finalize()
     }
 }
 
-/// Key Derivation Function
-pub fn kdf<H: Hasher32 + Clone>(key: &[u8], path: &[&[u8]]) -> [u8; 32] {
-    let mut current: Box<dyn Hasher32> =
-        Box::new(RecursiveHash::new(b"VMess AEAD KDF", H::clone(&H::clone(&Sha256Hash::new()))));
+pub fn kdf(key: &[u8], path: &[&[u8]]) -> [u8; 32] {
+    let mut current = Box::new(RecursiveHash::new(
+        b"VMess AEAD KDF",
+        Box::new(Sha256Hash::new()),
+    ));
 
-    for p in path {
-        current = Box::new(RecursiveHash::new(p, current.finalize_hasher()));
+    for p in path.iter() {
+        current = Box::new(RecursiveHash::new(*p, current));
     }
 
     current.update(key);
     current.finalize()
 }
 
-// Ekstensi trait untuk mendapatkan hasher baru dari output lama (tanpa Box)
-trait FinalizeHasher: Hasher32 {
-    fn finalize_hasher(&self) -> Self {
-        self.clone()
-    }
-}
-
-impl<T: Hasher32 + Clone> FinalizeHasher for T {}
-
-/// Optional helper: md5(key1 + key2) untuk UUID
-#[macro_export]
-macro_rules! md5 {
-    ($a:expr, $b:expr) => {{
-        use md5::Digest;
-        let mut hasher = md5::Md5::new();
-        hasher.update($a);
-        hasher.update($b);
-        hasher.finalize()
-    }};
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use md5::Md5;
 
     #[test]
     fn test_kdf() {
-        use uuid::uuid;
+        let uuid = uuid::uuid!("96850032-1b92-46e9-a4f2-b99631456894").as_bytes();
+        let key = crate::md5!(&uuid, b"c48619fe-8f02-49e0-b9e9-edf763e17e21");
 
-        let uuid = uuid!("96850032-1b92-46e9-a4f2-b99631456894").as_bytes();
-        let key = crate::md5!(uuid, b"c48619fe-8f02-49e0-b9e9-edf763e17e21");
-
-        let result = kdf::<Sha256Hash>(&key, &[b"AES Auth ID Encryption"]);
+        let res = kdf(&key, &[b"AES Auth ID Encryption"]);
 
         assert_eq!(
-            &result[..16],
-            &[117, 82, 144, 159, 147, 65, 74, 253, 91, 74, 70, 84, 114, 118, 203, 30]
+            res[..16],
+            [117, 82, 144, 159, 147, 65, 74, 253, 91, 74, 70, 84, 114, 118, 203, 30]
         );
     }
 }
