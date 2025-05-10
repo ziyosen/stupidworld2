0e4fbcc4714722cb23c0c1c81202ecc740883a9c
StupidX2 <luckyzp77@gmail.com>
2025-05-10 20:09:22 +0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -22,21 +22,11 @@ async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
         .var("UUID")
         .map(|x| Uuid::parse_str(&x.to_string()).unwrap_or_default())?;
     let host = req.url()?.host().map(|x| x.to_string()).unwrap_or_default();
-    let main_page_url = env.var("MAIN_PAGE_URL").map(|x| x.to_string()).unwrap();
-    let sub_page_url = env.var("SUB_PAGE_URL").map(|x| x.to_string()).unwrap();
-    let link_page_url = env.var("LINK_PAGE_URL").map(|x| x.to_string()).unwrap();
-    let convert_page_url = env.var("CONVERT_PAGE_URL").map(|x| x.to_string()).unwrap();
-
-    let config = Config { 
-        uuid, 
-        host: host.clone(), 
-        proxy_addr: host, 
-        proxy_port: 443, 
-        main_page_url, 
-        sub_page_url,
-        link_page_url,
-        convert_page_url
-    };
+    let main_page_url = env.var("MAIN_PAGE_URL").map(|x|x.to_string()).unwrap();
+    let sub_page_url = env.var("SUB_PAGE_URL").map(|x|x.to_string()).unwrap();
+    let link_page_url = env.var("LINK_PAGE_URL").map(|x|x.to_string()).unwrap();
+    let convert_page_url = env.var("CONVERT_PAGE_URL").map(|x|x.to_string()).unwrap();
+    let config = Config { uuid, host: host.clone(), proxy_addr: host, proxy_port: 443, main_page_url, sub_page_url, link_page_url, convert_page_url };
 
     Router::with_data(config)
         .on_async("/", fe)
@@ -74,12 +64,12 @@ async fn convert(_: Request, cx: RouteContext<Config>) -> Result<Response> {
 async fn tunnel(req: Request, mut cx: RouteContext<Config>) -> Result<Response> {
     let mut proxyip = cx.param("proxyip").unwrap().to_string();
     if PROXYKV_PATTERN.is_match(&proxyip)  {
-        let kvid_list: Vec<String> = proxyip.split(",").map(|s| s.to_string()).collect();
+        let kvid_list: Vec<String> = proxyip.split(",").map(|s|s.to_string()).collect();
         let kv = cx.kv("SIREN")?;
         let mut proxy_kv_str = kv.get("proxy_kv").text().await?.unwrap_or("".to_string());
         let mut rand_buf = [0u8, 1];
         getrandom::getrandom(&mut rand_buf).expect("failed generating random number");
-
+        
         if proxy_kv_str.len() == 0 {
             console_log!("getting proxy kv from github...");
             let req = Fetch::Url(Url::parse("https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/kvProxyList.json")?);
@@ -91,37 +81,37 @@ async fn tunnel(req: Request, mut cx: RouteContext<Config>) -> Result<Response> 
                 return Err(Error::from(format!("error getting proxy kv: {}", res.status_code())));
             }
         }
-
+        
         let proxy_kv: HashMap<String, Vec<String>> = serde_json::from_str(&proxy_kv_str)?;
-
+        
+        // select random KV ID
         let kv_index = (rand_buf[0] as usize) % kvid_list.len();
         proxyip = kvid_list[kv_index].clone();
-
+        
+        // select random proxy ip
         let proxyip_index = (rand_buf[0] as usize) % proxy_kv[&proxyip].len();
         proxyip = proxy_kv[&proxyip][proxyip_index].clone().replace(":", "-");
     }
 
-    if PROXYIP_PATTERN.is_match(&proxyip) {
+    let upgrade = req.headers().get("Upgrade")?.unwrap_or_default();
+    if upgrade == "websocket".to_string() && PROXYIP_PATTERN.is_match(&proxyip) {
         if let Some((addr, port_str)) = proxyip.split_once('-') {
             if let Ok(port) = port_str.parse() {
                 cx.data.proxy_addr = addr.to_string();
                 cx.data.proxy_port = port;
             }
         }
-    }
-
-    let upgrade = req.headers().get("Upgrade")?.unwrap_or("".to_string());
-    if upgrade == "websocket".to_string() {
+        
         let WebSocketPair { server, client } = WebSocketPair::new()?;
         server.accept()?;
-
+    
         wasm_bindgen_futures::spawn_local(async move {
             let events = server.events().unwrap();
             if let Err(e) = ProxyStream::new(cx.data, &server, events).process().await {
-                console_log!("[tunnel]: {}", e);
+                console_error!("[tunnel]: {}", e);
             }
         });
-
+    
         Response::from_websocket(client)
     } else {
         Response::from_html("hi from wasm!")
